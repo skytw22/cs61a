57096d2cbba47806430d50898681b482f1bc9935
Tianwen <skyyang0202@gmail.com>
2023-07-04 23:06:45 +0800

    completed hw08

---
+++ hw/hw08/tests/derive-sum.py
@@ -0,0 +1,51 @@
+test = {
+  'name': 'derive-sum',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          scm> (make-sum 1 3)
+          4
+          scm> (make-sum 'x 0)
+          x
+          scm> (make-sum 0 'x)
+          x
+          scm> (make-sum 'a 'x)
+          (+ a x)
+          scm> (make-sum 'a (make-sum 'x 1))
+          (+ a (+ x 1))
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': r"""
+      scm> (load-all ".")
+      """,
+      'teardown': '',
+      'type': 'scheme'
+    },
+    {
+      'cases': [
+        {
+          'code': r"""
+          scm> (derive '(+ x 3) 'x)
+          38dac9033a8f5e8edb2dbe6428e02d1d
+          # locked
+          """,
+          'hidden': False,
+          'locked': True
+        }
+      ],
+      'scored': True,
+      'setup': r"""
+      scm> (load-all ".")
+      """,
+      'teardown': '',
+      'type': 'scheme'
+    }
+  ]
+}
